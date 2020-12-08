488c85bdc4fc8a15f896b5ce71e3f2777eabda56
Nikola Gaic <nikolagaic@yahoo.com>
2020-12-08 12:48:37 +0100

    Create the event model

---
+++ EventHub/Models/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace EventHub.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        //if I want to query lists of some model, I need to add it here
+
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+         
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
