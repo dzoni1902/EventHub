9bd33d5bb4761d2f23ab7fa9370a7f8fc943914c
Nikola Gaic <nikolagaic@yahoo.com>
2020-12-08 16:23:01 +0100

    Create Add an Event form.

---
+++ EventHub/Controllers/EventsController.cs
@@ -0,0 +1,29 @@
+﻿using EventHub.Models;
+using EventHub.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace EventHub.Controllers
+{
+    public class EventsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EventsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+
+
+        public ActionResult Create()
+        {
+            var viewModel = new EventFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ EventHub/ViewModels/EventFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using EventHub.Models;
+using System.Collections.Generic;
+
+namespace EventHub.ViewModels
+{
+    public class EventFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }  //type is int - because of dropdown list (GenreId in DB)
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
