4f9653f7955474dff4b6c0d16f8c15b1b9aa1189
Nikola Gaic <nikolagaic@yahoo.com>
2020-12-14 10:52:17 +0100

    Implement listing of artists I follow

---
+++ EventHub/Controllers/FoloweesController.cs
@@ -0,0 +1,29 @@
+﻿using EventHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace EventHub.Controllers
+{
+    public class FoloweesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FoloweesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [Authorize]
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            var artists = _context.Followings
+                .Where(f => f.FollowerId == userId)
+                .Select(f => f.Followee)
+                .ToList();
+
+            return View(artists);
+        }
+    }
+}
