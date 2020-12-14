f069a7ca438e877066722e610717112db36b05b5
Nikola Gaic <nikolagaic@yahoo.com>
2020-12-14 15:41:15 +0100

    Implement Update for my Events.

--- EventHub/Controllers/EventsController.cs
+++ EventHub/Controllers/EventsController.cs
@@ -17,6 +17,28 @@ namespace EventHub.Controllers
             _context = new ApplicationDbContext();
         }
 
+        //Edit & Create actions take us to page where we CREATE new or EDIT existing event
+        //these actions return VIEW's, GET actions
+        [Authorize]
+        public ActionResult Edit(int id)
+        {
+            var userId = User.Identity.GetUserId();
+            var eventObject = _context.Events.Single(e => e.Id == id && e.ArtistId == userId);
+
+            //we need to initialize viewModel in order form to be populated in Edit page
+            var viewModel = new EventFormViewModel
+            {
+                Id = eventObject.Id,
+                Heading = "Edit an Event",
+                Genres = _context.Genres.ToList(),
+                Date = eventObject.DateTime.ToString("d MMM yyyy"),
+                Time = eventObject.DateTime.ToString("HH:mm"),
+                Genre = eventObject.GenreId,
+                Venue = eventObject.Venue
+            };
+
+            return View("EventForm", viewModel);
+        }
 
         //Action for clicking on Add an Event link in navbar
         [Authorize]
@@ -24,12 +46,14 @@ namespace EventHub.Controllers
         {
             var viewModel = new EventFormViewModel
             {
-                Genres = _context.Genres.ToList()
+                Genres = _context.Genres.ToList(),
+                Heading = "Add an Event",
             };
 
-            return View(viewModel);
+            return View("EventForm", viewModel);
         }
 
+        //CREATE and UPDATE actions are POST actions and they SAVE form data to the DB
         //convert viewModel to Event object, save to context (DB)
         [Authorize]
         [HttpPost]
@@ -41,7 +65,7 @@ namespace EventHub.Controllers
                 //we need to initialize and populate Genres prop since viewModel is 
                 //new model initialized with values from the httpRequest
                 viewModel.Genres = _context.Genres.ToList();
-                return View("Create", viewModel);
+                return View("EventForm", viewModel);
             }
 
             var eventObject = new Event
@@ -53,6 +77,31 @@ namespace EventHub.Controllers
             };
 
             _context.Events.Add(eventObject);
+            _context.SaveChanges();
+
+            return RedirectToAction("Mine", "Events");
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Update(EventFormViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                //we need to initialize and populate Genres prop since viewModel is 
+                //new model initialized with values from the httpRequest
+                viewModel.Genres = _context.Genres.ToList();
+                return View("EventForm", viewModel);
+            }
+
+            var userId = User.Identity.GetUserId();
+            var eventObject = _context.Events.Single(e => e.Id == viewModel.Id && e.ArtistId == userId);
+
+            eventObject.Venue = viewModel.Venue;
+            eventObject.DateTime = viewModel.GetDateTime();
+            eventObject.GenreId = viewModel.Genre;
+
             _context.SaveChanges();
 
             return RedirectToAction("Mine", "Events");
--- EventHub/ViewModels/EventFormViewModel.cs
+++ EventHub/ViewModels/EventFormViewModel.cs
@@ -10,6 +10,7 @@ namespace EventHub.ViewModels
         //view model is used for UI, to prevent adding new stuff and polute our domain classes
         //validations for UI fields go here, as attributes on props, then modification of
         //controller and view as well.
+        public int Id { get; set; }
 
         [Required]
         public string Venue { get; set; }
@@ -26,6 +27,10 @@ namespace EventHub.ViewModels
         public byte Genre { get; set; }  //type is int - because of dropdown list (GenreId in DB)
 
         public IEnumerable<Genre> Genres { get; set; } = new List<Genre>();
+
+        public string Heading { get; set; }
+
+        public string Action => (Id != 0) ? "Update" : "Create";
 
         //convert prop to method to avoid error caused by Reflection, when MVC calls Create
         //action and uses Reflection to recreate viewModel
