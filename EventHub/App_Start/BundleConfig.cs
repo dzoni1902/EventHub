42426a095852669c8cc1e9f087da8d5a8c8250de
Nikola Gaic <nikolagaic@yahoo.com>
2020-12-23 11:32:06 +0100

    Small refactor to the BundleConfig.cs

--- EventHub/App_Start/BundleConfig.cs
+++ EventHub/App_Start/BundleConfig.cs
@@ -7,11 +7,14 @@ namespace EventHub
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            bundles.Add(new ScriptBundle("~/bundles/lib").Include(
                         "~/Scripts/underscore-min.js",
                         "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/moment.js"));
+                        "~/Scripts/moment.js",
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/bootbox.min.js"));
 
+            //we need this bundle only on pages where we have form and validations
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
@@ -19,10 +22,6 @@ namespace EventHub
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/bootbox.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
